1dd4d66b1d1ba035728a0ccac0cb10888dfccb7b
BrahmaSunny <sunnymech307@gmmail.com>
2025-09-05 11:39:20 +0530

    test

--- Shell/01.sh
+++ Shell/01.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-num1 = 100
-num2 = 200
-sum = $((num1 + num2))
-echo "sum of two number is $num1 & $num2 = $sum".
+num1=100
+num2=200
+sum=$((num1 + num2))
+echo "sum of two number is $num1 and $num2 = $sum"
